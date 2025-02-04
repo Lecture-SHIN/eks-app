a23fb7e66736333c47c7ddfc098b52570a779bb0
Lecture-SHIN <lecture.yonggishin@gmail.com>
2025-02-04 09:17:30 +0900

    update feature:actuator

--- src/main/java/basic/controller/K8SApiController.java
+++ src/main/java/basic/controller/K8SApiController.java
@@ -3,11 +3,15 @@ package basic.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class K8SApiController {
 	
 	@GetMapping(value = "/api/status")
 	public String k8sApiTest() {
+		log.info("");
 		return "Status - returned by Pod";
 	}
 	
@@ -31,4 +35,8 @@ public class K8SApiController {
 		return "Status5 - returned by Pod";
 	}
 	
+	@GetMapping(value = "/api/status6")
+	public String k8sApiTest6() {
+		return "Status6 - returned by Pod";
+	}
 }
